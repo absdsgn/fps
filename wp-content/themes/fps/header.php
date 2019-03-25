e84fd3d9251016c2362ebbe18c364933a0fe2f0b
Andrew Becker <absdsgn@gmail.com>
2019-03-24 21:33:33 -0400

    under construction dev

--- wp-content/themes/fps/header.php
+++ wp-content/themes/fps/header.php
@@ -1,21 +1,10 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package fps
- */
-
-?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 	<?php wp_head(); ?>
 
     <link rel="stylesheet" href="wp-content/themes/fps/style.min.css">
--- wp-content/themes/fps/page-10.php
+++ wp-content/themes/fps/page-10.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area>
+	<div id="primary" class="content-area">
 		<?php
 		while ( have_posts() ) :
 			the_post();
